b4610c04c53ff4ef939ca79391a4a1ac6c6f7ede
bpsoos <soos.peter.96@gmail.com>
2024-11-16 22:34:07 +0100

    refactors

--- day3/parse_schematic.go
+++ day3/parse_schematic.go
@@ -15,23 +15,19 @@ func ParseSchematic(schmaticReader io.Reader) *Schematic {
 	for scanner.Scan() {
 		line := []rune(scanner.Text())
 		x := 0
+
 		for x < len(line) {
 			if line[x] == '.' {
 				x++
 				continue
 			}
+
 			if unicode.IsDigit(line[x]) {
-				value, length := textprocessing.MustGetNumber(line, x)
-				schematic.PartNumbers = append(schematic.PartNumbers, PartNumber{
-					Value: value,
-					Position: PartNumberPosition{
-						Start: mathx.Point{X: x, Y: y},
-						End:   mathx.Point{X: x + length - 1, Y: y},
-					},
-				})
+				length := addPartNumber(schematic, line, x, y)
 				x += length
 				continue
 			}
+
 			schematic.AddSymbol(mathx.Point{X: x, Y: y})
 			x++
 		}
@@ -39,4 +35,15 @@ func ParseSchematic(schmaticReader io.Reader) *Schematic {
 	}
 
 	return schematic
+}
+func addPartNumber(schematic *Schematic, line []rune, x int, y int) int {
+	value, length := textprocessing.MustGetNumber(line, x)
+	schematic.PartNumbers = append(schematic.PartNumbers, PartNumber{
+		Value: value,
+		Position: PartNumberPosition{
+			Start: mathx.Point{X: x, Y: y},
+			End:   mathx.Point{X: x + length - 1, Y: y},
+		},
+	})
+	return length
 }
