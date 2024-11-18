2027eeee6c34b932c72c22deddf4d50676706553
bpsoos <soos.peter.96@gmail.com>
2024-11-18 23:43:43 +0100

    adds solution for day 6 part 1

--- day6/win_strategizer.go
+++ day6/win_strategizer.go
@@ -10,12 +10,24 @@ func NewWinStrategizer(timeSheet []RaceTime) *WinStrategizer {
 
 func (ws WinStrategizer) WinPossibilities() []int {
 	possibilities := make([]int, len(ws.TimeSheet))
+
 	for i := 0; i < len(ws.TimeSheet); i++ {
-		possibilities[i] = ws.possibilitiesForRace(ws.TimeSheet[i])
+		possibilities[i] = ws.getPossibilityCount(ws.TimeSheet[i])
 	}
+
 	return possibilities
 }
 
-func (ws WinStrategizer) possibilitiesForRace(race RaceTime) int {
-	return 0
+func (ws WinStrategizer) getPossibilityCount(race RaceTime) int {
+	possibilityCount := 0
+
+	for holdTime := range race.Time {
+		runTime := race.Time - holdTime
+		distance := runTime * holdTime
+		if distance > race.Distance {
+			possibilityCount++
+		}
+	}
+
+	return possibilityCount
 }
